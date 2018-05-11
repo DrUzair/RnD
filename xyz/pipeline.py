fab34921087d06ffe8f6da2ab3fbeef17ec325dd
Uzair Ahmad <DrUzair@users.noreply.github.com>
2018-05-10 21:57:15 -0400

    Add files via upload

---
+++ xyz/pipeline.py
@@ -0,0 +1,59 @@
+""" Rubikloud take home problem """
+import luigi
+
+
+class CleanDataTask(luigi.Task):
+    """ Cleans the input CSV file by removing any rows without valid geo-coordinates.
+
+        Output file should contain just the rows that have geo-coordinates and
+        non-(0.0, 0.0) files.
+    """
+    tweet_file = luigi.Parameter()
+    output_file = luigi.Parameter(default='clean_data.csv')
+
+    # TODO...
+
+
+class TrainingDataTask(luigi.Task):
+    """ Extracts features/outcome variable in preparation for training a model.
+
+        Output file should have columns corresponding to the training data:
+        - y = airline_sentiment (coded as 0=negative, 1=neutral, 2=positive)
+        - X = a one-hot coded column for each city in "cities.csv"
+    """
+    tweet_file = luigi.Parameter()
+    cities_file = luigi.Parameter(default='cities.csv')
+    output_file = luigi.Parameter(default='features.csv')
+
+    # TODO...
+
+
+class TrainModelTask(luigi.Task):
+    """ Trains a classifier to predict negative, neutral, positive
+        based only on the input city.
+
+        Output file should be the pickle'd model.
+    """
+    tweet_file = luigi.Parameter()
+    output_file = luigi.Parameter(default='model.pkl')
+
+    # TODO...
+
+
+class ScoreTask(luigi.Task):
+    """ Uses the scored model to compute the sentiment for each city.
+
+        Output file should be a four column CSV with columns:
+        - city name
+        - negative probability
+        - neutral probability
+        - positive probability
+    """
+    tweet_file = luigi.Parameter()
+    output_file = luigi.Parameter(default='scores.csv')
+
+    # TODO...
+
+
+if __name__ == "__main__":
+    luigi.run()
